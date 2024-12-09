b48353dcd3188f25f72be62ed74ab03bded23386
PRATHA BAGTHARIA <124965525+Pratha3@users.noreply.github.com>
2024-12-09 17:45:06 +0530

    portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,220 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Markdown from "react-markdown";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      {/* Hero Section */}
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+
+      {/* Education Section */}
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.institution}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.institution}
+                title={education.institution}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+                logoUrl={education.logoUrl} // Add this
+                altText={education.institution} // Add this
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+
+      {/* Skills Section */}
+      <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Projects Section */}
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact" className="py-16 bg-gray-50 dark:bg-gray-900">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-8">
+            <BlurFade delay={BLUR_FADE_DELAY * 16}>
+              <div className="inline-block px-4 py-2 bg-primary text-white rounded-lg text-sm uppercase tracking-wider">
+                Let's Connect
+              </div>
+              <h2 className="text-4xl font-bold mt-4 text-gray-900 dark:text-white">
+                We’d Love to Hear From You
+              </h2>
+              <p className="text-gray-600 dark:text-gray-300 mt-2">
+                Feel free to reach out with any questions, feedback, or just to
+                say hello. Fill out the form below, and we'll get back to you
+                shortly!
+              </p>
+            </BlurFade>
+          </div>
+
+          <div className="max-w-2xl mx-auto">
+            <BlurFade delay={BLUR_FADE_DELAY * 17}>
+              <form
+                className="space-y-6 bg-white dark:bg-gray-800 p-8 shadow-lg rounded-lg"
+                action="/api/contact"
+                method="POST"
+              >
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter your name"
+                    className="mt-1 w-full p-3 border rounded-lg focus:ring-primary focus:border-primary dark:bg-gray-700 dark:text-gray-200 dark:focus:ring-primary-light dark:focus:border-primary-light"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    className="mt-1 w-full p-3 border rounded-lg focus:ring-primary focus:border-primary dark:bg-gray-700 dark:text-gray-200 dark:focus:ring-primary-light dark:focus:border-primary-light"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    name="message"
+                    id="message"
+                    placeholder="Write your message"
+                    rows={5}
+                    className="mt-1 w-full p-3 border rounded-lg focus:ring-primary focus:border-primary dark:bg-gray-700 dark:text-gray-200 dark:focus:ring-primary-light dark:focus:border-primary-light"
+                    required
+                  ></textarea>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full bg-primary text-white py-3 rounded-lg hover:bg-primary-dark dark:bg-primary-light dark:hover:bg-primary transition duration-300"
+                >
+                  Send Message
+                </button>
+              </form>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,161 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Pratha Bagtharia",
+  initials: "PB",
+  location: "Surat , Gujarat",
+  description:
+    "Full-Stack Developer passionate about creating innovative solutions and empowering others. Always eager to learn and grow.",
+  summary:
+    "I started my journey in full-stack development with a passion for building dynamic web applications. Over time, I have successfully completed my full-stack course, working on numerous projects that showcase my skills in both front-end and back-end development. I am excited to keep growing and applying my knowledge to create impactful solutions",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Bootstrap",
+    "Tailwind CSS",
+    "Redux",
+    "Firebase",
+    "MongoDB",
+    "Node.js",
+    "C++",
+  ],
+  navbar: [{ href: "/", icon: HomeIcon, label: "Home" }],
+  contact: {
+    email: "pratha19bagtharia@gmail.com",
+    tel: "+917383884149",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/Pratha3",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/bpratha11193",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/3Pratha",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+    },
+  },
+
+  education: [
+    {
+      institution: "Red and White Multimedia Education",
+      degree: "Full-stack Web Development",
+      start: "2023",
+      end: "2024",
+      logoUrl: "/path/to/logo.png", // Add the logo URL
+    },
+    {
+      institution: "Vadodara Institute of Engineering",
+      degree: " Bachelor of Engineering in Computer Science (BE)",
+      start: "2021",
+      end: "2023",
+      logoUrl: "/path/to/logo.png", // Add the logo URL
+    },
+    {
+      institution: "Sigma Public School",
+      degree: "High School",
+      start: "2019",
+      end: "2021",
+      logoUrl: "/path/to/logo.png", // Add the logo URL
+    },
+  ],
+
+  projects: [
+    {
+      title: "Firebase Auth",
+      href: "https://firebase-auth-two-phi.vercel.app/",
+      dates: "Custom Project Timeline",
+      active: true,
+      description:
+        "Implemented user authentication using Firebase, focusing on secure login, signup, and password reset functionalities.",
+      technologies: ["Firebase", "JavaScript", "HTML", "CSS"],
+      links: [
+        {
+          type: "Website",
+          href: "https://firebase-auth-two-phi.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/Pratha3/Firebase-Auth",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
+    },
+    {
+      title: "Furniture",
+      href: "https://furniture-zn96.onrender.com",
+      dates: "Custom Project Timeline",
+      active: true,
+      description:
+        "Developed an e-commerce furniture website with a responsive design and smooth navigation.",
+      technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+      links: [
+        {
+          type: "Website",
+          href: "https://furniture-zn96.onrender.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/Pratha3/Furniture",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
+    },
+    {
+      title: "Movie Swatch",
+      href: "https://github.com/Pratha3/movie-swatch",
+      dates: "Custom Project Timeline",
+      active: true,
+      description:
+        "Built a movie browsing platform featuring advanced search and filtering capabilities for a better user experience.",
+      technologies: ["Node.js", "Express.js", "MongoDB", "EJS"],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/Pratha3/movie-swatch",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
+    },
+    {
+      title: "HTTP Server",
+      href: "https://github.com/Pratha3/http-server",
+      dates: "Custom Project Timeline",
+      active: true,
+      description:
+        "Created a basic HTTP server to handle requests and responses, showcasing understanding of web protocols.",
+      technologies: ["Node.js", "JavaScript"],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/Pratha3/http-server",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "",
+    },
+  ],
+} as const;
